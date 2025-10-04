4a89a6c83941531de96f1fae2b66421417f9cf28
JonaSeguReymundo <00043823@uca.edu.sv>
2025-10-03 22:13:50 -0600

    GET and POST requests for the /api/boards endpoint working successfully.

--- AppWebCanvan/routes/api.php
+++ AppWebCanvan/routes/api.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\BoardController;
 use App\Http\Controllers\ColumnController;
 use App\Http\Controllers\TaskController;
 
-Route::prefix('api')->middleware('api')->group(function () {
-    Route::apiResource('boards', BoardController::class);
-    Route::apiResource('columns', ColumnController::class);
-    Route::apiResource('tasks', TaskController::class);
-});
+Route::apiResource('boards', BoardController::class);
+Route::apiResource('columns', ColumnController::class);
+Route::apiResource('tasks', TaskController::class);
